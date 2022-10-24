2fa1b53d2f4258ff32964e147b554f2ec37e0d05
Julia Simonenko <juliasimonenko99@gmail.com>
2022-10-24 17:11:42 +0300

    new letter

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,5 +1,5 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, EffectCube } from 'swiper';
+Swiper.use([Navigation, Pagination, EffectCube]);
 
 const heroSlider = new Swiper('.hero-slider', {
   slidesPerView: 1,
@@ -10,10 +10,29 @@ const heroSlider = new Swiper('.hero-slider', {
     clickable: true,
   },
 });
+
 const contactSlider = new Swiper('.contact-slider', {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const blogSlider = new Swiper('.blog-slider', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
